1d0d3d73e791217ee1d4964c9c358f3bd3d66376
内藤祐馬 <naitoyuma@outlook.jp>
2022-11-24 17:24:33 +0900

    Composition-api injection/probiderのテスト

---
+++ src/components/TestInjection.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>{{ userId }}</p>
+</template>
+
+<script>
+import { inject } from "vue";
+export default {
+  setup() {
+    // userIdでinjectを実行し,provideされたglobalな値を取得する
+    const userId = inject("userId");
+
+    return {
+      userId,
+    };
+  },
+};
+</script>
