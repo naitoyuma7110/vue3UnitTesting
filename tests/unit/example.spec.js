2c9f1c6d527779fcc53214a8d0a0cb67324a9884
内藤祐馬 <naitoyuma@outlook.jp>
2022-11-24 13:33:21 +0900

    testファイル移動

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,14 @@
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
 
-describe("HelloWorld.vue", () => {
+describe("HelloWorld.vueの初期テスト", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
+    // wrapperにマウントした際、propsにmsgを渡す
     const wrapper = shallowMount(HelloWorld, {
       props: { msg },
     });
+    // text()でレンダリング時のテキストを取得し、渡したmsgが表示されているか確認
     expect(wrapper.text()).toMatch(msg);
   });
 });
